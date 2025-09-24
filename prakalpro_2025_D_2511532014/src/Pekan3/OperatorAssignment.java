bffaa5b964f2ae12e9de0ce5b61d8a66493fea80
Ryan Al Ghifari <ryanalghifari.ra@gmail.com>
2025-09-24 15:56:31 +0700

    Pekan3

---
+++ prakalpro_2025_D_2511532014/src/Pekan3/OperatorAssignment.java
@@ -0,0 +1,30 @@
+package Pekan3;
+import java.util.Scanner;
+public class OperatorAssignment {
+	public static void main(String[] args) {
+		int A1;
+			int A2;
+			Scanner keyboard = new Scanner(System.in);
+			System.out.print("Input angka-1: ");
+			A1 = keyboard.nextInt();// input 10
+			System.out.print("Input angka-2: ");
+			A2 = keyboard.nextInt();// input 5
+			keyboard.close();
+			System.out.println("Assignment penambahan");
+			A1 += A2;//penambahan, sekarang A1=15
+			System.out.println("Penambahan : "+ A1);
+			System.out.println("Assignment pengurangan");
+			A1 -= A2;//pengurangan, sekarang A1=5
+			System.out.println("Pengurangan : "+ A1);
+			System.out.println("Assignment perkalian");
+			A1 *= A2;//perkalian, sekrang A1=50
+			System.out.println("Perkalian : "+ A1);
+			System.out.println("Assignment hasil bagi");
+			A1 /= A2;//hasil bagi, sekaranh A1=3
+			System.out.println("Pembagian : "+ A1);
+			System.out.println("Assignment sisa bagi");
+			A1 %= A2;//sisa bagi, sekarang A1=0
+			System.out.println("Sisa bagi : "+ A1);
+	}
+
+}
