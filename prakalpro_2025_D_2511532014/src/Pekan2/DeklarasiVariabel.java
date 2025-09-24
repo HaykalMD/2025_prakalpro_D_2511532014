b1ee494b22d6504f5ba35d682886a53acec2d095
Ryan Al Ghifari <ryanalghifari.ra@gmail.com>
2025-09-24 14:19:23 +0700

    Pekan2

---
+++ prakalpro_2025_D_2511532014/src/Pekan2/DeklarasiVariabel.java
@@ -0,0 +1,22 @@
+package Pekan2;
+
+public class DeklarasiVariabel {
+	/* program java
+	 * latihan
+	 * tentang pendeklarasian variabel
+	 */
+	static int umur=25; /* variabel langsung dapat diinisasi */
+	public static void main(String[] args) {
+		int kode;
+		boolean isDibawahumur; /* perhatikan penulisan nama variabel */
+		kode = 1234; /* pengisian variabel (assignment */
+		double gaji; /* deklarasi variabel dapat dimana saja */
+		gaji = 5500000.23;
+		isDibawahumur = true;
+		System.out.println("Status: "+isDibawahumur);
+		System.out.println("Kode: "+kode);
+		System.out.println("Umur: "+umur);
+		System.out.println("Gaji: "+ gaji);
+	}
+
+}
