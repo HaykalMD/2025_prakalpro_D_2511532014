9013ee9ffad25a5d8ac0801addb388c13fae37e9
Haykal <haykalmdahnil@gmail.com>
2025-09-28 21:46:54 +0700

    Add files via upload

---
+++ prakalpro_2025_D_2511532014/src/Pekan2/alproTugasPekan2.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		// Deklarasi variabel
+        int usia = 16;
+        float berat = 35.7f;
+        char jenis = 'M';
+        boolean dilindungi = true;
+        
+        // Output
+        System.out.println("== Data Hewan di Kebun Binatang ==");
+        System.out.println("Usia: "+ usia);
+        System.out.println("Berat: "+ berat);
+        System.out.println("Jenis: "+ jenis);
+        System.out.println("Dilindungi: "+ dilindungi);
+	}
+}
